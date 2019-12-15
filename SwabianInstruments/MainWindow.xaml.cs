dc3d532b7c875a5e71e274397f366c80b4e4700c
ahassan <ahassan@onica.com>
2019-12-15 10:28:31 +0200

    Refine layout - Add oxyplot graph plotter - test the plotter

--- SwabianInstruments/MainWindow.xaml.cs
+++ SwabianInstruments/MainWindow.xaml.cs
@@ -12,9 +12,31 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using OxyPlot;
 
 namespace SwabianInstruments
 {
+    public class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.Title = "Example 2";
+            this.Points = new List<DataPoint>
+                              {
+                                  new DataPoint(0, 4),
+                                  new DataPoint(10, 13),
+                                  new DataPoint(20, 15),
+                                  new DataPoint(30, 16),
+                                  new DataPoint(40, 12),
+                                  new DataPoint(50, 12)
+                              };
+        }
+
+        public string Title { get; private set; }
+
+        public IList<DataPoint> Points { get; private set; }
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
